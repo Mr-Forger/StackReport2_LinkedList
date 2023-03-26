bdb4a2228aba1e26a71e327a393df3cd21be1246
SeungHan Moon <msh9911@naver.com>
2023-03-26 23:17:05 +0900

    초기 커밋.

---
+++ StackReport2/LinkedListStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackReport2
+{
+    internal class LinkedListStack
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> stack = new LinkedList<int>();
+
+            //push
+            stack.AddLast(0);
+            stack.AddLast(1);
+            stack.AddLast(2);
+
+            //pop
+            Console.WriteLine(stack.Last.Value);
+            stack.RemoveLast();
+            Console.WriteLine(stack.Last.Value);
+            stack.RemoveLast();
+            Console.WriteLine(stack.Last.Value);
+            stack.RemoveLast();
+            Console.WriteLine(stack.Last.Value);
+
+        }
+    }
+}
